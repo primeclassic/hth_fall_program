54333d6bbc8fb3100a96601b43514aa184b8de3f
primeclassic <106497414+primeclassic@users.noreply.github.com>
2022-10-06 18:39:06 -0700

    completing first python file

---
+++ Getting-Started-with-Python-primeclassic/about_me.py
@@ -0,0 +1,7 @@
+name = "Zhi Cong Xu"
+nickname = "Hugo"
+age = 17
+Used_Python == True
+favorite_hobby = "watching anime"
+favorite_things = ["boba","video games","weight lifting"]
+all_vars = dict(vars())
